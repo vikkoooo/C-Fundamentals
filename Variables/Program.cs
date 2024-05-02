ce93d430afbfbc7167dffd08b525d14101252144
vikkoooo <vikkolundberg@gmail.com>
2024-05-02 13:32:27 +0200

    Understanding Data Types and Variables

---
+++ Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Variables
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int y;
+			int x;
+
+			x = 7;
+			y = x + 3;
+
+			Console.WriteLine($"y = {y}");
+
+			Console.WriteLine("What is your name?");
+			Console.Write("Type your first name: ");
+			string myFirstName = Console.ReadLine();
+
+			Console.Write("Type your last name: ");
+			string myLastName = Console.ReadLine();
+
+			Console.WriteLine($"Hello, {myFirstName} {myLastName}");
+		}
+	}
+}
