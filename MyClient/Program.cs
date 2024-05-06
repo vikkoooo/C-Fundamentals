e1bdb4aca75c2c5ec5e82e409cce8a3f01cbda01
vikkoooo <vikkolundberg@gmail.com>
2024-05-06 12:18:42 +0200

    creating nd dding references to assemblies

---
+++ MyClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using MyCodeLibrary;
+
+namespace MyClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Scrape myScrape = new Scrape();
+			string value = myScrape.ScrapeWebpage("www.google.com");
+			Console.WriteLine(value);
+		}
+	}
+}
---
+++ MyCodeLibrary/Scrape.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Runtime.InteropServices;
+
+namespace MyCodeLibrary;
+public class Scrape
+{
+	public string ScrapeWebpage(string url)
+	{
+		return getWebpage(url);
+	}
+
+	public string ScrapeWebpage(string url, string filepath)
+	{
+		string reply = getWebpage(url);
+
+		File.WriteAllText(filepath, reply);
+		return reply;
+	}
+
+	private string getWebpage(string url)
+	{
+		WebClient client = new WebClient();
+		return client.DownloadString(url);
+	}
+
+}
