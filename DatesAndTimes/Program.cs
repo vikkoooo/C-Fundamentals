a024fb552b70889d77ed3592df655241ae025494
vikkoooo <vikkolundberg@gmail.com>
2024-05-06 09:43:08 +0200

    working with dates and times

---
+++ DatesAndTimes/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DatesAndTimes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DateTime myValue = DateTime.Now;
+			//Console.WriteLine(myValue.ToString());
+			//Console.WriteLine(myValue.ToShortDateString());
+			//Console.WriteLine(myValue.ToShortTimeString());
+			//Console.WriteLine(myValue.ToLongDateString());
+			//Console.WriteLine(myValue.ToLongTimeString());
+
+			//Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+			//Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+			//Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+
+			//Console.WriteLine(myValue.Month);
+
+			//DateTime myBirthday = new DateTime(1969, 12, 7);
+			//Console.WriteLine(myBirthday.ToShortDateString());
+
+			DateTime myBirthday = DateTime.Parse("12/7/1969");
+			TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+			Console.WriteLine(myAge.TotalDays);
+		}
+	}
+}
