bc8fbcee6f87b37ce5c1ff61a9c9ce7810f06cf1
vikkoooo <vikkolundberg@gmail.com>
2024-05-10 14:33:56 +0200

    understanding events

---
+++ TimerExample/TimerExample.cs
@@ -0,0 +1,32 @@
+﻿namespace TimerExample
+{
+	class TimerExample
+	{
+		static void Main(string[] args)
+		{
+			Timer myTimer = new Timer(2000);
+
+			myTimer.Elapsed += MyTimer_Elapsed;
+			myTimer.Elapsed += MyTimer_Elapsed1;
+
+			myTimer.Start();
+
+			Console.WriteLine("Press enter to remove the red event");
+			Console.ReadLine();
+
+			myTimer.Elapsed -= MyTimer_Elapsed1;
+		}
+
+		private static void MyTimer_Elapsed1(object sender, ElapsedEventArgs e)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Elapsed: {0:HH:mm:ss:fff}", e.SignalTime);
+		}
+
+		private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("Elapsed: {0:HH:mm:ss:fff}", e.SignalTime);
+		}
+	}
+}
