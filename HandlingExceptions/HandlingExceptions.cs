b1ed476b0711f7e863900f9324713b4e54a1c97d
vikkoooo <vikkolundberg@gmail.com>
2024-05-13 17:02:42 +0200

    gracefully handling exceptions

---
+++ HandlingExceptions/HandlingExceptions.cs
@@ -0,0 +1,33 @@
+﻿namespace HandlingExceptions
+{
+	class HandlingExceptions
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				string content = File.ReadAllText(@"PATH"); // path
+				Console.WriteLine(content);
+			}
+			catch (FileNotFoundException ex)
+			{
+				Console.WriteLine("There was a problem!");
+				Console.WriteLine("Make sure the file name is named correctly: Example.txt");
+			}
+			catch (DirectoryNotFoundException ex)
+			{
+				Console.WriteLine("There was a problem!");
+				Console.WriteLine(@"Make sure the dictory c:\users\... exists");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("there was a problem");
+				Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				Console.WriteLine("Closing the app now...");
+			}
+		}
+	}
+}
