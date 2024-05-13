a6c97da511bc8bdcc07f6f228dce8c80decbb201
vikkoooo <vikkolundberg@gmail.com>
2024-05-13 17:11:13 +0200

    understanding namespaces

---
+++ AssembliesAndNamespaces/AssembliesAndNamespaces.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace AssembliesAndNamespaces
+{
+	class AssembliesAndNamespaces
+	{
+		static void Main(string[] args)
+		{
+			string text = "Write text to file";
+			File.WriteAllText(@"path", text);
+
+			WebClient client = new WebClient();
+			string reply = client.DownloadString("http://msdn.microsoft.com");
+
+			Console.WriteLine(reply);
+			File.WriteAllText(@"path", reply);
+		}
+	}
+}
