08cff615807302f27846c42bb2c3ec2258ff8009
vikkoooo <vikkolundberg@gmail.com>
2024-05-13 17:06:20 +0200

    operators, expressions and statements

---
+++ OperatorsExpressionsStatements/OperatorsExpressionsStatements.cs
@@ -0,0 +1,48 @@
+﻿namespace OperatorsExpressionsStatements
+{
+	class OperatorsExpressionsStatements
+	{
+		static void Main(string[] args)
+		{
+			int x, y, a, b;
+
+			x = 3;
+			y = 2;
+			a = 1;
+			b = 0;
+
+			x = 3 + 4;
+
+			x = 10 - 5;
+
+			x = 3 * 5;
+
+			x = 10 / 5;
+
+			x = (x + y) * (a - b);
+
+			if (x == y)
+			{
+			}
+
+			if (x > y)
+			{
+			}
+
+			if (x < y)
+			{
+			}
+
+			if ((x > y) && (a > b))
+			{
+			}
+
+			if ((x > y) || (a > b))
+			{
+			}
+
+			string message = (x == 1) ? "Car" : "Boat";
+			Console.WriteLine(message);
+		}
+	}
+}
